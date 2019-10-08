44eab3a2ba6476e9f270de265028fa29b5a6be13
Xingzhao.H <huang.xing.zhao@qq.com>
2019-10-08 12:30:15 +0800

    三维重建

--- SCUT_RoboLab_ros.cpp
+++ SCUT_RoboLab_ros.cpp
@@ -34,7 +34,6 @@ main()
     {
         rRect.push_back(cv::minAreaRect(i));
     }
-
     for (auto &i : rRect)
     {
         if (i.size.width < i.size.height)
@@ -55,18 +54,66 @@ main()
         }
     }
 
-    
-    if(rRect.size()!=2)
-        cv::waitKey(0);
-    for (auto i : rRect)
+    std::vector<cv::Point2f> corner;
+    for (auto i :rRect)
     {
         cv::Point2f vertices[4];
         i.points(vertices);
-        for (int j = 0; j < 4; j++)
-        {			
-            line(frame, vertices[j], vertices[(j + 1) % 4], cv::Scalar(255, 0, 0), 2);
+        if(rRect[0].center.x>rRect[1].center.x?!corner.empty():corner.empty())
+        {
+            corner.push_back(vertices[0]);
+            corner.push_back(vertices[3]);
+        }
+        else
+        {
+            corner.push_back(vertices[2]);
+            corner.push_back(vertices[1]);
         }
     }
+    const std::vector<cv::Point3f>corner_w{cv::Point3f(-6.75,-2.75,0),
+                                                                                        cv::Point3f(-6.75,2.75,0),
+                                                                                        cv::Point3f(6.75,2.75,0),
+                                                                                        cv::Point3f(6.75,-2.75,0)};
+
+    // cv::Mat corner_c(2,4,CV_64FC1);
+    // for(size_t i=0;i<4;++i)
+    // {
+    //     corner_c.ptr(0)[i]=(float)corner[i].x;
+    //     corner_c.ptr(1)[i]=(float)corner[i].y;
+    // }
+    // const cv::Mat corner_w = (cv::Mat_<float>(3,4)<<-6.75,  -6.75,  6.75,   6.75,
+    //                                                                                                     -2.75,  2.75,   2.75,  -2.75,
+    //                                                                                                             0,        0,         0,          0);
+    const cv::Mat cameraM = (cv::Mat_<float>(3,3)<<1128.048344,0                            ,339.421769,
+                                                                                                        0                       ,1127.052190    ,236.535242,
+                                                                                                        0                       ,0                            ,1);
+    const cv::Mat distCoeffs = (cv::Mat_ <float>(1,5)<<-0.568429,0.514592,-0.000126,0.000500,0);
+    cv::Mat Rvec(3,1,CV_64FC1),Tvec(3,1,CV_64FC1);
+    cv::Mat rotMat(3,3,CV_64FC1);
+    cv::solvePnP(corner_w,corner,cameraM,distCoeffs,Rvec,Tvec);
+    cv::Rodrigues(Rvec,rotMat);
+    Rvec.convertTo(Rvec,CV_64FC1);
+    Tvec.convertTo(Tvec,CV_64FC1);
+    rotMat.convertTo(rotMat,CV_64FC1);
+   // cv::transpose(Tvec,Tvec);
+    std::vector<cv::Point3f>corner_cam(corner_w);
+    for(auto &i:corner_cam)
+    {
+        cv::Mat buf(1,3,CV_64FC1);
+        cv::Mat(i).convertTo(buf,CV_64FC1);
+        buf = rotMat*buf+Tvec;
+        //std::cout<<( rotMat*buf+Tvec)<<std::endl;
+        i=cv::Point3f(buf);
+        std::cout<<i<<std::endl;
+    }
+    //rotMat.inv();
+    
+    //std::cout<<Tvec<<std::endl;
+    cv::waitKey();
+        for (int j = 0; j < 4; j++)
+        {			
+            line(frame, corner[j], corner[(j + 1) % 4], cv::Scalar(255, 0, 0), 2);
+        }
         imshow("frame",frame);
         cv::waitKey(30);
     }
